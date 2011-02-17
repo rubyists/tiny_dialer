eb222e6ab7a4d450498e4dfa4a3603e1b61791a4
Rubyists <rubyists@rubyists.com>
2011-02-17 13:15:28 -0600

    Cubs Popper

--- lib/tiny_dialer/direct_listener.rb
+++ lib/tiny_dialer/direct_listener.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 require 'fsr'
 require 'fsr/listener/outbound'
+require 'fsr/listener/inbound'
 require_relative "../../app"
 
 module TinyDialer
@@ -26,6 +27,7 @@ module TinyDialer
         @lead = Lead[:id => id]
         if lead
           direct_transfer(exten, queue)
+          tell_popper(exten, lead)
         else
           FSR::Log.info "No lead found for id: #{id}, hanging up"
           hangup
@@ -46,14 +48,68 @@ module TinyDialer
       end
     end
 
+    def tell_popper(exten, lead)
+      return unless popper = POPPER[exten]
+      popper.popup(lead)
+    end
+  end
+
+  class DialEventListener < FSR::Listener::Inbound
+    def before_session
+      add_event :CUSTOM, 'callcenter::info', &method(:callcenter_info)
+      add_event :BACKGROUND_JOB, &method(:background_job)
+    end
+
+    def callcenter_info(event)
+      content = event.content
+
+      case content[:cc_action]
+      when 'bridge-agent-start'
+        bridge_agent_start(content)
+      end
+    end
+
+    def background_job(event)
+      FSR::Log.debug "<<< Bridge Agent Start >>>"
+      FSR::Log.debug event
+    end
+
+    def bridge_agent_start(content)
+      FSR::Log.debug "!!! Bridge Agent Start !!!"
+      FSR::Log.debug content
+    end
+  end
+
+  module CubsPopper
+    POPPERS = {}
+
+    def receive_data(data)
+      case data
+      when /^extension: (\d+)/
+        @extension = $1
+        FSR::Log.info "Register Cubs Popper for #{@extension}"
+        POPPERS[@extension] = self
+      end
+    end
+
+    def unbind
+      FSR::Log.info "Disconnect Cubs Popper for #{@extension}"
+      POPPERS.delete @extension
+    end
+
+    def popup(lead)
+      send_data "(#{lead})"
+    end
   end
 end
 
 options = TinyDialer.options.direct_listener
 
-FSR.start_oes!(
-  TinyDialer::DirectListener,
-  port: options.port,
-  host: options.host,
-  queue_server: options.tcc_server
-)
+EventMachine::run do
+  FSR::Log.info "Start CUBS Popper Server on 0.0.0.0:9186"
+  EventMachine::start_server "0.0.0.0", 9186, TinyDialer::CubsPopper
+
+  FSR::Log.info "Connecting to #{options.host} #{options.port}"
+  EventMachine::connect options.host, options.port, TinyDialer::DialEventListener, host: options.host, port: options.port
+  EventMachine::connect options.host, options.port, TinyDialer::DirectListener, host: options.host, port: options.port, queue_server: options.tcc_server
+end
